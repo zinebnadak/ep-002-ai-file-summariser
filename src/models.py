3752cc3664fa81adb1a2da21f70cdf3e8e9c1760
zinebnadak <zineb@nadak.ai>
2026-05-23 19:28:46 +0300

    feat: add Pydantic models for ChunkSummary and FileSummary

--- src/models.py
+++ src/models.py
@@ -1,4 +1,4 @@
-# What a Chunck and file summury looks like, then Pydantic validates datatypes using its Basemodel, and w
+# What a Chunck and file summury looks like, then Pydantic validates datatypes using its Basemodel
 
 from pydantic import BaseModel  #checks if data types are valid 
 from typing import List #fields contain MULTIPLE items, just just a single str , using only 
@@ -11,7 +11,7 @@ class ChunckSummary (BaseModel):
 class Filesummary (BaseModel):
     title: str
     overview: str
-    key_sections: list[Section] # list o dictionaries, each item is a dictionary BUT validate the dict is a str
+    key_sections: list[Section] # list o dictionaries, each item is a dictionary BUT validate the dict is a str with Section
 
 class Section (BaseModel):
     sectio_title: str
