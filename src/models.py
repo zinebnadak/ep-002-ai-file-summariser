ad52c78373c3f10c352c2b382fb147f6595268ec
zizo <zizo@zizos-MacBook-Air.local>
2026-05-27 12:15:54 +0300

    fix:correct typos and types in models

--- src/models.py
+++ src/models.py
@@ -3,16 +3,17 @@
 from pydantic import BaseModel  #checks if data types are valid 
 from typing import List #fields contain MULTIPLE items, just just a single str , using only 
 
-class ChunckSummary (BaseModel):
+class ChunkSummary (BaseModel):
     main_concept: str
-    key_points: str
-    keywords: str
+    key_points: list[str]
+    keywords: list[str]
 
-class Filesummary (BaseModel):
+class Section (BaseModel):
+    section_title: str
+    summary: str
+
+class FileSummary (BaseModel):
     title: str
     overview: str
     key_sections: list[Section] # list o dictionaries, each item is a dictionary BUT validate the dict is a str with Section
 
-class Section (BaseModel):
-    sectio_title: str
-    summary: str
